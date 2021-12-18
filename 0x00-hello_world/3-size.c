9888829d3e9bd284323a80638454625b7fbb7b49
root <yurialvesfr21@gmail.com>
2021-12-18 22:45:04 +0000

    final commit for task 3

---
+++ 0x00-hello_world/3-size.c
@@ -0,0 +1,14 @@
+/**
+* main - entry points
+* Return: always 0, sucess
+*/
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 bytes(s)\n");
+	printf("Size of a long int: 4 bytes(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	return (0);
+}
