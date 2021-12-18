31f21b3f5b0a8f8c8efe468d62c1742c113acdbf
root <yurialvesfr21@gmail.com>
2021-12-18 21:17:39 +0000

    final update in task 1

---
+++ 0x00-hello_world/1-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str[15];
+
+	strcpy(str, "Programming is like building a multilingual puzzle\n");
+
+	puts(str);
+
+	return (0);
+}
