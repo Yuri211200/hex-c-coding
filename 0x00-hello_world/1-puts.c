57e998b2983b22ec4f86a8c72a4ee5d77715b3f5
root <yurialvesfr21@gmail.com>
2021-12-18 22:02:41 +0000

    update in task 1

--- 0x00-hello_world/1-puts.c
+++ 0x00-hello_world/1-puts.c
@@ -1,10 +1,10 @@
 /**
 * main - entry points
-* Return - always 0, sucess
+* Return: always 0, sucess
 */
 #include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
