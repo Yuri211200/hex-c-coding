5d769917094aaa8378cfff341a4825b7be7e1e6d
root <yurialvesfr21@gmail.com>
2021-12-18 21:52:34 +0000

    update in task 1

--- 0x00-hello_world/1-puts.c
+++ 0x00-hello_world/1-puts.c
@@ -5,7 +5,7 @@ int main(void)
 {
 	char str[52];
 
-	strcpy(str, "Programming is like building a multilingual puzzle");
+	strcpy(str, "Programming is like building a multilingual puzzle\n");
 
 	puts(str);
 
